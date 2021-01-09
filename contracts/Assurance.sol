4587606778a2f0c19c60db1cecae5e3fadf04322
Mars <email@genesis.re>
2021-01-09 10:23:10 +0000

    Simplifying...

--- contracts/Assurance.sol
+++ contracts/Assurance.sol
@@ -8,96 +8,59 @@ import "../node_modules/@chainlink/contracts/src/v0.6/interfaces/AggregatorInter
 contract Assurance is Ownable {
     using SafeMath for uint256;
 
-    address payable beneficiary;
-    ERC20PresetMinterPauser public BSXO; // we assume 1 $BSXO = 1 USD
     AggregatorInterface public oracle;
-    uint USD;
+    address payable beneficiary; // MULTISIG
+    ERC20PresetMinterPauser public BSXO;
+    mapping (address => uint) public balances;
 
-    mapping (address => uint) public investorsWEImap;
-    mapping (address => uint) public investorsUSDmap;
-
-    address[] investorsWEIarr;
-    address[] investorsUSDarr;
-
-    function investorsWEICount() public view returns(uint) {
-        return investorsWEIarr.length;
-    }
-
-    function investorsUSDCount() public view returns(uint) {
-        return investorsUSDarr.length;
-    }
-    
     constructor(address oracleAddress, address BSXOaddress, address payable _beneficiary) public {
         beneficiary = _beneficiary;
         BSXO = ERC20PresetMinterPauser(BSXOaddress);
         oracle = AggregatorInterface(oracleAddress);
-        lastTimeInterest = now;
     }
 
-    uint public lastTimeInterest;
-    function accrueInterest() public {
-        // require(now - lastTimeInterest > 1 days, "Interest is accounted max 1 per day"); // ACTUALLY: does not matter, can accrue anytime
+    // uint public lastTimeInterest;
+    // function accrueInterest() public {
+    //     // require(now - lastTimeInterest > 1 days, "Interest is accounted max 1 per day"); // ACTUALLY: does not matter, can accrue anytime
 
-        uint timeSinceTheLastTime = now - lastTimeInterest;
-        uint baseInterestRate = currentValue() > 10000000 ? 2000 : 10000; // More than $100k, the interest rate is 20%, below is 100%
+    //     uint timeSinceTheLastTime = now - lastTimeInterest;
+    //     uint baseInterestRate = currentValue() > 10000000 ? 2000 : 10000; // More than $100k, the interest rate is 20%, below is 100%
 
 
-        uint arrayLength = investorsWEIarr.length;
-        for (uint i=0; i<arrayLength; i++) {
-            address investorAddress = investorsWEIarr[i];
-            uint principalUSDvalue = getUSDValueOfWEI(investorsWEImap[investorAddress]);
+    //     uint arrayLength = investorsWEIarr.length;
+    //     for (uint i=0; i<arrayLength; i++) {
+    //         address investorAddress = investorsWEIarr[i];
+    //         uint principalUSDvalue = getUSDValueOfWEI(balances[investorAddress]);
 
-            uint interestBSXO = principalUSDvalue.mul(timeSinceTheLastTime).div(365 days).mul(baseInterestRate).div(10000); // here we have dollar value
+    //         uint interestBSXO = principalUSDvalue.mul(timeSinceTheLastTime).div(365 days).mul(baseInterestRate).div(10000); // here we have dollar value
 
-            uint BSXODecimals = interestBSXO.mul(10000000000000000); // here we have the the value with accurate number of decimals
-            BSXO.mint(investorAddress, BSXODecimals);
-        }
+    //         uint BSXODecimals = interestBSXO.mul(10000000000000000); // here we have the the value with accurate number of decimals
+    //         BSXO.mint(investorAddress, BSXODecimals);
+    //     }
 
-        arrayLength = investorsUSDarr.length;
-        for (uint i=0; i<arrayLength; i++) {
-            address investorAddress = investorsUSDarr[i];
-            uint principalUSDvalue = investorsUSDmap[investorAddress];
+    //     lastTimeInterest = now;
+    // }
 
-            uint interestBSXO = principalUSDvalue.mul(timeSinceTheLastTime).div(365 days).mul(baseInterestRate).div(10000);
-
-            uint BSXODecimals = interestBSXO.mul(10000000000000000); // here we have the the value with accurate number of decimals
-            BSXO.mint(investorAddress, BSXODecimals);
-        }
-
-        lastTimeInterest = now;
-    }
-
-    function getUSDValueOfWEI(uint WEI) public returns (uint) {
+    function getUSDValueOfWEI(uint WEI) public view returns (uint) {
         uint price = (uint)(oracle.latestAnswer());
-        return WEI.div(1 finney).mul(price).div(1000000000);
+        return WEI.mul(price).div(1000000000000000000000000); // getting the right decimals Chainlink represents data
     }
 
-    function currentValue() public returns(uint) {
-        return getUSDValueOfWEI(address(this).balance).add(USD);
-    }
-
-    function manuallyDeposit(address benefactor, string memory proof, uint amount) public onlyOwner {
-        if(investorsUSDmap[benefactor] == 0) {
-            investorsUSDarr.push(benefactor); // keeping unique count of investors in the array
-        }
-
-        investorsUSDmap[benefactor] = investorsUSDmap[benefactor].add(amount);
-        USD = USD.add(amount);
+    function currentValue() public view returns(uint) {
+        return getUSDValueOfWEI(address(this).balance);
     }
 
     receive() external payable {
-        if(investorsWEImap[msg.sender] == 0) {
-            investorsWEIarr.push(msg.sender); // keeping unique count of investors in the array
-        }
-        investorsWEImap[msg.sender] = investorsWEImap[msg.sender].add(msg.value);
+        balances[msg.sender] = balances[msg.sender].add(msg.value);
+    }
+
+    function withdraw() public {
+        msg.sender.transfer(balances[msg.sender]);
     }
 
     function closeAndBuyTheIslandOrTheCollege() public onlyOwner {
         selfdestruct(beneficiary);
     }
 
-    function chuckNorrisSaidOK() public onlyOwner {
-        BSXO.unpause();
-    }
 
 }
--- test/assurance.js
+++ test/assurance.js
@@ -34,81 +34,47 @@ contract('Assurance contract to buy island and college', async function(accounts
         await assurance.sendTransaction({ value: toWei("0.05"), from: guy1 });
         await assurance.sendTransaction({ value: toWei("0.17"), from: guy2 });
 
-        let investorCount = await assurance.investorsWEICount.call()
-        assert.equal(investorCount, 2, "There should be to investors already");
-
         let balance = await assurance.currentValue.call();
         assert.equal(balance.toString(), 8611, "0.22 ETH at $391.45 shold be $86.11");
     });
 
-    it('Can calculate money invested in USD', async () => {
-        await assurance.manuallyDeposit(guy1, "ipfs_hash", 10050, { from: creator });
-        await assurance.manuallyDeposit(guy2, "ipfs_hash", 20070, { from: creator });
-        await assurance.manuallyDeposit(guy3, "ipfs_hash", 30090, { from: creator });
+    // it('Can generate interest when one guy send ETH', async () => {
+    //     await assurance.sendTransaction({ value: toWei("99"), from: guy1 });
 
-        let investorCount = await assurance.investorsUSDCount.call()
-        assert.equal(investorCount, 3, "There should be to investors already");
-
-        let balance = await assurance.currentValue.call();
-        assert.equal(balance.toString(), 60210, "Three guys investing money, should be $602.11");
-
-        await assurance.manuallyDeposit(guy3, "ipfs_hash", 790, { from: creator });
-        investorCount = await assurance.investorsUSDCount.call()
-        assert.equal(investorCount, 3, "There should be to investors already");
-
-        balance = await assurance.currentValue.call();
-        assert.equal(balance.toString(), 61000, "Now it should be a little bit more, should be $610.00");
-    });
-
-    it('Can calculate money invested in ETH and USD', async () => {
-        await assurance.sendTransaction({ value: toWei("0.05"), from: guy1 });
-        await assurance.sendTransaction({ value: toWei("0.17"), from: guy2 });
-
-        await assurance.manuallyDeposit(guy1, "ipfs_hash", 10050, { from: creator });
-        await assurance.manuallyDeposit(guy2, "ipfs_hash", 20070, { from: creator });
-        await assurance.manuallyDeposit(guy3, "ipfs_hash", 30090, { from: creator });
-
-        let balance = await assurance.currentValue.call();
-        assert.equal(balance.toString(), 68821, "Three guys investing money, should be $688.21");
-    });
-
-    it('Can generate interest when one guy send ETH', async () => {
-        await assurance.sendTransaction({ value: toWei("99"), from: guy1 });
-
-        await increaseTime(2 * days);
+    //     await increaseTime(2 * days);
         
-        await assurance.accrueInterest();
+    //     await assurance.accrueInterest();
 
-        // 99 ETH at $391.45 is $38,753.55
-        // 2 days of interest at 100% per year (we are below $100k) equals to $212.34 worth of $BSXO of interest
-        // 38,753.55 / 365 * 2 = 212.34 intuitively makes sense to me :) 
+    //     // 99 ETH at $391.45 is $38,753.55
+    //     // 2 days of interest at 100% per year (we are below $100k) equals to $212.34 worth of $BSXO of interest
+    //     // 38,753.55 / 365 * 2 = 212.34 intuitively makes sense to me :) 
 
-        let BSXObalance = await BSXO.balanceOf(guy1);
-        let BSXObalanceFromWEI = fromWei(BSXObalance);
-        assert.equal(BSXObalanceFromWEI, "212.34", "It should be exactly 212.34 BSXO");
-    });
+    //     let BSXObalance = await BSXO.balanceOf(guy1);
+    //     let BSXObalanceFromWEI = fromWei(BSXObalance);
+    //     assert.equal(BSXObalanceFromWEI, "212.34", "It should be exactly 212.34 BSXO");
+    // });
 
-    it('Can generate interest when two guys send USD (above $100k)', async () => {
-        await assurance.manuallyDeposit(guy1, "ipfs_hash", 10000000, { from: creator });
-        await assurance.manuallyDeposit(guy2, "ipfs_hash", 20000000, { from: creator });
+    // it('Can generate interest when two guys send USD (above $100k)', async () => {
+    //     await assurance.manuallyDeposit(guy1, "ipfs_hash", 10000000, { from: creator });
+    //     await assurance.manuallyDeposit(guy2, "ipfs_hash", 20000000, { from: creator });
 
-        await increaseTime(7 * days);
+    //     await increaseTime(7 * days);
         
-        await assurance.accrueInterest();
+    //     await assurance.accrueInterest();
 
-        // $100k and $200k
-        // 7 days of interest at 20% per year (above $100k) 
-        // 100000 * 0.2 * 7 / 365 = 383.56
-        // 200000 * 0.2 * 7 / 365 = 767.12
+    //     // $100k and $200k
+    //     // 7 days of interest at 20% per year (above $100k) 
+    //     // 100000 * 0.2 * 7 / 365 = 383.56
+    //     // 200000 * 0.2 * 7 / 365 = 767.12
 
-        let BSXObalance1 = await BSXO.balanceOf(guy1);
-        let BSXObalanceFromWEI1 = fromWei(BSXObalance1);
-        assert.equal(BSXObalanceFromWEI1, "383.56", "Should be exactly 383.56");
+    //     let BSXObalance1 = await BSXO.balanceOf(guy1);
+    //     let BSXObalanceFromWEI1 = fromWei(BSXObalance1);
+    //     assert.equal(BSXObalanceFromWEI1, "383.56", "Should be exactly 383.56");
 
-        let BSXObalance2 = await BSXO.balanceOf(guy2);
-        let BSXObalanceFromWEI2 = fromWei(BSXObalance2);
-        assert.equal(BSXObalanceFromWEI2, "767.12", "Should be exacttly 767.12");
-    });
+    //     let BSXObalance2 = await BSXO.balanceOf(guy2);
+    //     let BSXObalanceFromWEI2 = fromWei(BSXObalance2);
+    //     assert.equal(BSXObalanceFromWEI2, "767.12", "Should be exacttly 767.12");
+    // });
 
     // THIS TEST IS NOT WORKING
     // FOR SOME REASON "expectThrow" FAILS :(
@@ -126,54 +92,54 @@ contract('Assurance contract to buy island and college', async function(accounts
     // });
 
 
-    it('Can generate when guys send ETH and USD, initially at 100% APR and then 20% APR only, total mix and match', async () => {
-        await assurance.manuallyDeposit(guy4, "ipfs_hash", 5000000, { from: creator }); // guy1 run out of ETH in one of the previous tests
-        await assurance.sendTransaction({ value: toWei("50"), from: guy2 });
+    // it('Can generate when guys send ETH and USD, initially at 100% APR and then 20% APR only, total mix and match', async () => {
+    //     await assurance.manuallyDeposit(guy4, "ipfs_hash", 5000000, { from: creator }); // guy1 run out of ETH in one of the previous tests
+    //     await assurance.sendTransaction({ value: toWei("50"), from: guy2 });
 
-        await increaseTime(7 * days);
-        await assurance.accrueInterest();
+    //     await increaseTime(7 * days);
+    //     await assurance.accrueInterest();
 
-        // $50k and 50 ETH
-        // 7 days of interest at 100% per year (below $100k) 
-        // 5000000 * 7 / 365 = 958.90
-        // 50ETH * 391.45 * 7 / 365 = 375.36
+    //     // $50k and 50 ETH
+    //     // 7 days of interest at 100% per year (below $100k) 
+    //     // 5000000 * 7 / 365 = 958.90
+    //     // 50ETH * 391.45 * 7 / 365 = 375.36
 
-        let BSXObalance1 = await BSXO.balanceOf(guy4);
-        let BSXObalanceFromWEI1 = fromWei(BSXObalance1);
-        assert.equal(BSXObalanceFromWEI1, "958.9", "Should be exactly 958.9"); // for display reasons prefer 958.90 but close enough LOL
+    //     let BSXObalance1 = await BSXO.balanceOf(guy4);
+    //     let BSXObalanceFromWEI1 = fromWei(BSXObalance1);
+    //     assert.equal(BSXObalanceFromWEI1, "958.9", "Should be exactly 958.9"); // for display reasons prefer 958.90 but close enough LOL
 
-        let BSXObalance2 = await BSXO.balanceOf(guy2);
-        let BSXObalanceFromWEI2 = fromWei(BSXObalance2);
-        assert.equal(BSXObalanceFromWEI2, "375.36", "Should be exacttly 375.36");
+    //     let BSXObalance2 = await BSXO.balanceOf(guy2);
+    //     let BSXObalanceFromWEI2 = fromWei(BSXObalance2);
+    //     assert.equal(BSXObalanceFromWEI2, "375.36", "Should be exacttly 375.36");
 
-        /////// PHASE 2
-        // We need to do some math now
-        // Loads of things changed
-        // 10 ETH from guy who already put $50k
-        // $10k from a guy who already put 50 ETH
-        await assurance.sendTransaction({ value: toWei("10"), from: guy4 });
-        await assurance.manuallyDeposit(guy2, "ipfs_hash", 1000000, { from: creator });
+    //     /////// PHASE 2
+    //     // We need to do some math now
+    //     // Loads of things changed
+    //     // 10 ETH from guy who already put $50k
+    //     // $10k from a guy who already put 50 ETH
+    //     await assurance.sendTransaction({ value: toWei("10"), from: guy4 });
+    //     await assurance.manuallyDeposit(guy2, "ipfs_hash", 1000000, { from: creator });
 
-        oracle.updateAnswer(140000000000); // now ETH is at $1400 ATH
+    //     oracle.updateAnswer(140000000000); // now ETH is at $1400 ATH
 
-        await increaseTime(10 * days);
-        await assurance.accrueInterest();
+    //     await increaseTime(10 * days);
+    //     await assurance.accrueInterest();
         
-        // 5000000 * 0.2 * 10 / 365 = 273.97
-        // 10ETH * 1400.00 * 10 / 365 = 76.71
-        // 958.9 (from the previous one) + 273.97 + 76.71 = 1,309.58
+    //     // 5000000 * 0.2 * 10 / 365 = 273.97
+    //     // 10ETH * 1400.00 * 10 / 365 = 76.71
+    //     // 958.9 (from the previous one) + 273.97 + 76.71 = 1,309.58
 
-        BSXObalance1 = await BSXO.balanceOf(guy4);
-        BSXObalanceFromWEI1 = fromWei(BSXObalance1);
-        assert.equal(BSXObalanceFromWEI1, "1309.58", "Should be exactly 1309.58");
+    //     BSXObalance1 = await BSXO.balanceOf(guy4);
+    //     BSXObalanceFromWEI1 = fromWei(BSXObalance1);
+    //     assert.equal(BSXObalanceFromWEI1, "1309.58", "Should be exactly 1309.58");
 
-        // 50ETH * 1400.00 * 10 / 365 = 383.56
-        // 1000000 * 0.2 * 10 / 365 = 54.79
-        // 375.36 + 383.56 + 54.79 = 813.71
-        BSXObalance2 = await BSXO.balanceOf(guy2);
-        BSXObalanceFromWEI2 = fromWei(BSXObalance2);
-        assert.equal(BSXObalanceFromWEI2, "813.71", "Should be exacttly 813.71");
-    });
+    //     // 50ETH * 1400.00 * 10 / 365 = 383.56
+    //     // 1000000 * 0.2 * 10 / 365 = 54.79
+    //     // 375.36 + 383.56 + 54.79 = 813.71
+    //     BSXObalance2 = await BSXO.balanceOf(guy2);
+    //     BSXObalanceFromWEI2 = fromWei(BSXObalance2);
+    //     assert.equal(BSXObalanceFromWEI2, "813.71", "Should be exacttly 813.71");
+    // });
 
 
     // THIS TEST ALSO DOES NOT WORK :(
